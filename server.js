cfd26540f182ab6a630a5a1d92f9ccbddef7a1fb
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-12-30 23:05:48 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express');
-const fs = require('fs');
 const PORT = 5050;
 const app = express();
 app.use(express.json());
 
 require("./mongoose_conn.js")
-app.get("/", async (req, res) => {
-    // const result =await  collection.find().toArray()
-    // res.json(result)
-});
 
-app.use(logger)
 const booksRouter = require("./routes/books");
-function readFile(){
-    let students = fs.readFileSync('./students.json');
-    students = JSON.parse(students);
-    return students
-}
-function logger(req,res,next){
-    req.students = readFile()
-    next()
-}
+
 app.use("/books",booksRouter)
 
 app.listen(PORT);
